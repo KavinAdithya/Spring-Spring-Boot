4cbf3c265c4437936e7c6a5355ae2ce80737a393
KaVin S <kavinadithya3@gmail.com>
2024-10-20 17:52:45 +0530

    Working On Bean Reuage and its two ways and Record classes

--- src/main/java/com/techcrack/LearningSpring/Application02HelloWorld.java
+++ src/main/java/com/techcrack/LearningSpring/Application02HelloWorld.java
@@ -6,6 +6,14 @@ public class Application02HelloWorld {
 	public static void main(String[] args) {
 		var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
 		
-		System.out.println(context.getBean("name"));
+		// System.out.println(context.getBean("name"));
+		
+		// Person person = (Person)context.getBean("person");
+		// person.displayData();
+		// System.out.println(person.toString());
+		
+		// System.out.println(context.getBean("address2"));
+		
+		// System.out.println(context.getBean("person1"));
 	}
 }
--- src/main/java/com/techcrack/LearningSpring/HelloWorldConfiguration.java
+++ src/main/java/com/techcrack/LearningSpring/HelloWorldConfiguration.java
@@ -3,6 +3,21 @@ package com.techcrack.LearningSpring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person(String name, int age , Address address) {
+	public void displayData() {
+		System.out.println("Company Name is " + name + "\nIt was Old for " + age + " Agoo.. \n" + address);	
+	}
+}
+
+record Address(String district, String state, String country) {
+	@Override
+	public String toString() {
+		return "District : " + district +
+				"\nState : " + state +
+				"\nCountry : " + country;
+	}
+}
+
 @Configuration
 public class HelloWorldConfiguration {
 	
@@ -10,4 +25,25 @@ public class HelloWorldConfiguration {
 	public String name() {
 		return "Techcrack";
 	}
-}
+	
+	@Bean
+	public int age() {
+		return 43;
+	}
+	
+	@Bean
+	public Person person() {
+		return new Person("Techiess", 19, null);
+	}
+	
+	@Bean(name = "address2")
+	public Address address() {
+		return new Address("Dharmapuri", "TamilNadu", "India");
+	}
+	
+	// Bean Reusing Existing
+	@Bean
+	public Person person1() {
+		return new Person(name(), age(), address());
+	}
+ }
